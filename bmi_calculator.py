27c70be8fb472e319a94d19a2212f6ea5c5032c7
nonnymox <knonxho@gmail.com>
2025-02-24 16:54:28 -0800

    Added interpretation of bmi ranges

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,5 +1,12 @@
 def calculate_bmi(height, weight):
     bmi = weight/height**2
+    if  bmi < 18.5:
+        print("Underweight")
+    elif bmi < 25:
+        print("Normal weight")
+    else:
+        print("Overweight")
+
     return bmi
 
 print(calculate_bmi(1.65,84))
